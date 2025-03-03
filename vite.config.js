113eca65767ba30e8d5591d2f7e775d9a6351480
minicoder99 <minicoder1986@gmail.com>
2025-03-03 16:27:11 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://notesapp-backend-theta.vercel.app:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
